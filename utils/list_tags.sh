c479c4a50554653c2be9dd29c8bfe6921a26fcf5
Laurent Cheylus <foxy@free.fr>
2026-07-17 15:36:05 +0200

    utils: add shell script to list tags for COSMIC repository
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

---
+++ utils/list_tags.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# List tags for https://github.com/pop-os/cosmic-epoch/ repository
+set -eu
+
+if ! command -v curl >/dev/null 2>&1; then
+	"ERROR: required command 'curl' is not installed or not in PATH."
+	exit 1
+fi
+
+if ! command -v jq >/dev/null 2>&1; then
+	"ERROR: required command 'jq' is not installed or not in PATH."
+	exit 1
+fi
+
+curl -sL -H "Accept: application/vnd.github+json" \
+	-H "X-GitHub-Api-Version: 2026-03-10" \
+	https://api.github.com/repos/pop-os/cosmic-epoch/tags \
+	| jq -r .[].name
+
