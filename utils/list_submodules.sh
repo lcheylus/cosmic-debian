4394cf0675f6c2987dbd4edeec35fd88f6bcfcde
Laurent Cheylus <foxy@free.fr>
2026-07-26 14:44:48 +0200

    utils: format shell scripts list_{tags,submodules}.sh
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- utils/list_submodules.sh
+++ utils/list_submodules.sh
@@ -14,6 +14,5 @@ fi
 
 curl -sL -H "Accept: application/vnd.github+json" \
 	-H "X-GitHub-Api-Version: 2026-03-10" \
-	https://api.github.com/repos/pop-os/cosmic-epoch/contents/ \
-	| jq '.[] | select(.type == "submodule")' | jq -r .name
-
+	https://api.github.com/repos/pop-os/cosmic-epoch/contents/ |
+	jq '.[] | select(.type == "submodule")' | jq -r .name
--- utils/list_tags.sh
+++ utils/list_tags.sh
@@ -14,6 +14,5 @@ fi
 
 curl -sL -H "Accept: application/vnd.github+json" \
 	-H "X-GitHub-Api-Version: 2026-03-10" \
-	https://api.github.com/repos/pop-os/cosmic-epoch/tags \
-	| jq -r .[].name
-
+	https://api.github.com/repos/pop-os/cosmic-epoch/tags |
+	jq -r .[].name
